0e47710b2e0afa9d82c55132281c3593d22769ae
Nithin Krishna <nithinkrishh@gmail.com>
2016-02-13 17:15:20 -0800

    appending time-stamp to file hash, paginated saving

--- download.py
+++ download.py
@@ -1,24 +1,62 @@
 from os import listdir, mkdir
 from os.path import isfile, join, isdir
 from shutil import copyfile
-
 from tika import detector
+from shutil import move
 
 import ntpath
 import re
-
 import sys
+import time
+
+import pdb
+
+
+class FileCountHandler:
+  def __init__(self, path):
+    self.file = join(path,"count")
+
+  def readCount(self):
+    return int(tuple(open(self.file, "r"))[0])
+
+  def writeCount(self, count):
+    f = open(self.file, "w+")
+    return f.write(str(count))
+
+  def incrementCount(self):
+    self.writeCount(self.readCount() + 1)
+
+def safeMakeDir(path):
+  if not isdir(path):
+    mkdir(path)
+
+def writeFile(folder, fileName, tmpPath):
+  countH = FileCountHandler(folder)
+  subFolder = countH.readCount() / 100
+  safeMakeDir(join(folder, str(subFolder)))
+  fPath = join(folder, str(subFolder), fileName)
+  move(tmpPath, fPath)
+  countH.incrementCount()
+  print "{0}".format(fPath)
 
 
 def fetchFile(path, END_POINT):
+  fileName = "{0}-{1}".format(ntpath.basename(path), time.time())
+
+  # Download File
+  tmpPath = join(END_POINT, fileName)
+  copyfile(path, tmpPath)
+
+  # Run Tika
   tp = detector.from_file(path).replace("/", "-")
-  fileName = ntpath.basename(path)
 
-  if not isdir(join(END_POINT, tp)):
-    mkdir(join(END_POINT, tp))
+  d = join(END_POINT, tp)
 
-  copyfile(path, join(END_POINT, tp, fileName))
-  print "{0} {1}".format(tp, fileName)
+  if not isdir(d):
+    safeMakeDir(d)
+    FileCountHandler(d).writeCount(0)
+
+  writeFile(d, fileName, tmpPath)
 
 
 def dfs_traversal(path, END_POINT):
