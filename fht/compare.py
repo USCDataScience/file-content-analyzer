58a34c593f708296109939a7ef85944e9f26e8e1
Nithin Krishna <nithinkrishh@gmail.com>
2016-02-15 23:29:26 -0800

    module restructuring

---
+++ fht/compare.py
@@ -0,0 +1,17 @@
+import math
+
+class CompareFHT:
+  def __init__(size,matrix,fingerprint):
+    self.size=size
+    self.matrix=matrix
+    self.fingerprint=matrix
+
+  def compare(self):
+    sum=0
+    denom=0
+    for i in range(size):
+      for j in range(256):
+        sum=sum+ self.matrix[i][j]*self.fingerprint[i][j]
+        denom=denom+self.fingerprint[i][j]
+
+  return (sum/denom)
