50068f73a3966a57c4d822642899faf558695a18
Ajay Kumar <aloganat@usc.edu>
2016-02-15 19:21:13 -0800

    Update FHT.py
    The arguments that should be passed are :
    Base signature
    No of previous files
    Header/ Trailer size
    Input file

--- FHT.py
+++ FHT.py
@@ -1,7 +1,7 @@
 import math
 
 class FileHeaderTrailer:
-	def __init__(self,baseSignature,no_of_files,byte,file1):
+	def __init__(self,baseSignature,prev_files,byte,file1):
 		self.baseSignature=baseSignature
 		self.byte=byte
 		self.file=file1
@@ -9,7 +9,7 @@ class FileHeaderTrailer:
 	def fht(self):
 		signature = Matrix= [[0 for x in range(256)]for x in range(self.byte)]
 		size=len(self.file)
-		for i in range(byte):
+		for i in range(self.byte):
 			for j in range(256):
 				no= self.file.read(8)
 				if(j==no):
@@ -20,9 +20,9 @@ class FileHeaderTrailer:
 					signature[i][j]=-1
 
 		newSignature=Matrix= [[0 for x in range(256)]for x in range(self.byte)]
-		for i in range(byte):
+		for i in range(self.byte):
 			for j in range(256):
-				newSignature=((baseSignature[i][j]*prev_files)+ signature[i][j])/(prev_files+1)
+				newSignature=((self.baseSignature[i][j]*self.prev_files)+ signature[i][j])/(self.prev_files+1)
 
 
 	return newSignature
